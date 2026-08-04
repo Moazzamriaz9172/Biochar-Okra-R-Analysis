1e328d9ee1313966edb501c132c63fa4342f35ca
Moazzam Riaz <moazzamriaz.official@gmail.com>
2026-08-04 17:17:27 +0500

    Rename R project to biochar_okra.Rproj

---
+++ 01_import_data.R
@@ -0,0 +1,29 @@
+install.packages("janitor")
+install.packages("here")
+
+library(tidyverse)
+library(readxl)
+library(janitor)
+library(here)
+ here()
+ file_path <- here(
+   "data",
+   "raw",
+   "okra_biochar_data.xlsx"
+ ) 
+sheet_names <- excel_sheets(file_path)
+ 
+sheet_names
+
+library(purrr)
+
+raw_data <- map(
+  sheet_names,
+  ~read_excel(file_path, sheet = .x)
+)
+
+names(raw_data) <- sheet_names
+
+map(raw_data, dim)
+
+View(raw_data[[1]])
